10453ccccb53595031362983279dc351e5e981df
Nathan Walters <nwalters512@gmail.com>
2019-02-22 10:56:27 -0600

    Layout updates (#11)
    * Update layout and responsiveness
    
    * Remove header component

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,25 @@ import React from 'react'
 
 import { Button, Container, Row, Col, Badge } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBolt, faChartBar, faVideo, faChalkboardTeacher, faUsers, faBan } from '@fortawesome/free-solid-svg-icons'
+import { faBolt, faChartBar, faChalkboardTeacher, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faSlack } from '@fortawesome/free-brands-svg-icons'
 
-import Header from '../components/Header'
 import RandomQueue from '../components/RandomQueue'
 
 export default () => (
   <>
-    <Header />
     <Container fluid className="bg-dark py-3">
-      <Row>
-        <Col md="6">
-          <h1 className="text-light">The Illinois Queue</h1>
-          <h4 className="text-light">The Queue allows students, instructors, advisors, and more to be productive and utilize their time more effectively.</h4>
-          <div className="mt-4">
-            <Button color="primary" className="mr-4 mb-4" tag="a" href="https://queue.illinois.edu/q/">Go directly to the Queues @ Illinois ➤</Button>
-            <br />
-            <Button color="secondary" className="mb-4" tag="a" href="https://go.illinois.edu/new-queue/">Request a queue for your course ➤</Button>
+      <Row className="justify-content-center">
+        <Col sm="10" md="8" lg="6" className="d-flex flex-column justify-content-center py-3 py-md-5 py-lg-0 hero-text-column">
+          <h1 className="text-light mb-4">Queue@Illinois</h1>
+          <h4 className="text-light mb-4">The Queue allows students, instructors, advisors, and more to be productive and utilize their time more effectively.</h4>
+          <div>
+            <Button color="primary" className="m-2 ml-lg-0" tag="a" href="https://queue.illinois.edu/q/">Go to the Queue</Button>
+            <Button color="secondary" className="m-2" tag="a" href="https://go.illinois.edu/new-queue/">Request a queue</Button>
           </div>
         </Col>
-        <Col lg="2" className="d-none d-lg-block" />
-        <Col lg="4" md="6" className="d-none d-md-block">
+        <Col xl="1" className="d-none d-xl-block" />
+        <Col md="6" xl="5" className="d-none d-lg-block">
           <RandomQueue />
         </Col>
       </Row>
@@ -78,7 +75,7 @@ export default () => (
       <Container>
         <Row>
           <Col>
-            <h3>Open source</h3>
+            <h3>Open Source</h3>
             <p>
               Don&apos;t see the feature you want? The Queue is open-source,
               so anyone can contribute new features or bug fixes. Or, join the
@@ -103,9 +100,9 @@ export default () => (
           <h3 className="bottom-cta-header">Ready to get started?</h3>
           <p className="bottom-cta-copy">Sign in or request a queue for your course.</p>
         </Col>
-        <Col md="6" className="d-flex align-items-center justify-content-md-end">
-          <Button color="primary" className="mr-4 mb-4" tag="a" href="https://queue.illinois.edu/q/">Go to the Queue</Button>
-          <Button color="secondary" className="mb-4" tag="a" href="https://go.illinois.edu/new-queue/">Request a queue</Button>
+        <Col md="6" className="d-flex align-items-center justify-content-md-end flex-wrap">
+          <Button color="primary" className="mr-4 my-2 m-md-2" tag="a" href="https://queue.illinois.edu/q/">Go to the Queue</Button>
+          <Button color="secondary" className="my-2 m-md-2" tag="a" href="https://go.illinois.edu/new-queue/">Request a queue</Button>
         </Col>
       </Row>
       <div className="mt-4 text-center">
@@ -121,6 +118,14 @@ export default () => (
 
 
     <style jsx>{`
+      :global(.hero-text-column) {
+        text-align: center;
+      }
+      @media (min-width: 992px) {
+        :global(.hero-text-column) {
+          text-align: start;
+        }
+      }
       .dummy-video {
         width: 100%;
         max-width: 780px;
