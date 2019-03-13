5ccbc4e1bdde8b3be595f995def4271ff29550cb
Nathan Walters <nwalters512@gmail.com>
2019-03-13 03:34:53 -0500

    Show date on post preview

--- components/blog/PostPreview.jsx
+++ components/blog/PostPreview.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import formatDate from "date-fns/format";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 
 const PostPreview = ({ meta, slug }) => (
   <article>
     <header>
-      <h3><Link href={`/blog/${slug}`}>{meta.title}</Link></h3>
+      <h3>
+        <Link href={`/blog/${slug}`}>{meta.title}</Link>
+      </h3>
+      <small className="secondary">
+        {formatDate(meta.date, "dddd, MMMM Do YYYY")} (
+        {distanceInWordsToNow(meta.date, {
+          addSuffix: true
+        })}
+      </small>
     </header>
   </article>
 );
