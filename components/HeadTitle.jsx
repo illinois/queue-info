0883018639731343e0287e27fea428b6ce380e23
Jacqueline Osborn <jackieo5023@gmail.com>
2019-03-31 22:18:27 -0500

    Change title of page

--- components/HeadTitle.jsx
+++ components/HeadTitle.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
+import { isAfter } from 'date-fns'
 
+const isAprilFools = !isAfter('2019-04-02T00:00:00-0500')
 const HeadTitle = ({ title }) => (
   <Head>
     <title>
       {title}
-      {title && ' | '}Queue@Illinois
+      {title && ' | '}
+      {isAprilFools ? 'Stack@Illinois' : 'Queue@Illinois'}
     </title>
   </Head>
 )
